6674c0c3617d1bedec73ee564254f835560de705
if0ne <pavel.agafonov.al@gmail.com>
2024-05-21 12:41:11 +0300

    Added resource api

--- src/resources.rs
+++ src/resources.rs
@@ -1,12 +1,53 @@
-use windows::{core::Interface, Win32::Graphics::Direct3D12::ID3D12Resource};
+use std::ops::Range;
 
-use crate::{create_type, impl_trait, HasInterface};
+use windows::{
+    core::Interface,
+    Win32::Graphics::Direct3D12::{ID3D12Resource, D3D12_RANGE},
+};
 
-pub trait ResourceInterface: HasInterface<Raw: Interface> {}
+use crate::{create_type, error::DxError, impl_trait, HasInterface};
+
+pub trait ResourceInterface: HasInterface<Raw: Interface> {
+    //TODO: Type for pointer
+    fn map(&self, subresource: u32, read_range: Option<Range<usize>>) -> Result<*mut (), DxError>;
+    fn unmap(&self, subresource: u32, written_range: Option<Range<usize>>);
+}
 
 create_type! { Resource wrap ID3D12Resource }
 
 impl_trait! {
     impl ResourceInterface =>
     Resource;
+
+    fn map(&self, subresource: u32, read_range: Option<Range<usize>>) -> Result<*mut (), DxError> {
+        let mut ptr = std::ptr::null_mut();
+        let range = read_range.map(|r| D3D12_RANGE {
+            Begin: r.start,
+            End: r.end,
+        });
+    
+        unsafe {
+            self.0
+                .Map(
+                    subresource,
+                    range.as_ref().map(|r| r as *const _),
+                    Some(&mut ptr),
+                )
+                .map_err(|_| DxError::Dummy)?;
+        }
+    
+        Ok(ptr as *mut ())
+    }
+    
+    fn unmap(&self, subresource: u32, written_range: Option<Range<usize>>) {
+        let range = written_range.map(|r| D3D12_RANGE {
+            Begin: r.start,
+            End: r.end,
+        });
+    
+        unsafe {
+            self.0
+                .Unmap(subresource, range.as_ref().map(|r| r as *const _));
+        }
+    }
 }
