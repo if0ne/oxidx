85ca78d71007bbd96623473ede9d03e5eb5aff18
if0ne <pavel.agafonov.al@gmail.com>
2024-06-21 12:13:09 +0300

    Add: docs for execute command lists

--- oxidx/src/command_queue.rs
+++ oxidx/src/command_queue.rs
@@ -34,10 +34,10 @@ pub trait CommandQueueInterface:
     /// * `region_size` - A reference to a [`TileRegionSize`] structure that describes the size of the reserved region.
     ///
     /// # Remarks
-    /// Use [`CommandQueueInterface::copy_tile_mappings`] to copy the tile mappings from one reserved resource to another, either to duplicate a resource mapping,
+    /// Use [`copy_tile_mappings`] to copy the tile mappings from one reserved resource to another, either to duplicate a resource mapping,
     /// or to initialize a new mapping before modifying it using [`CommandQueueInterface::update_tile_mappings`].
     ///
-    /// [`CommandQueueInterface::copy_tile_mappings`] helps with tasks such as shifting mappings around within and across reserved resources, for example, scrolling tiles.
+    /// [`copy_tile_mappings`] helps with tasks such as shifting mappings around within and across reserved resources, for example, scrolling tiles.
     /// The source and destination regions can overlap; the result of the copy in this situation is as
     /// if the source was saved to a temporary location and from there written to the destination.
     ///
@@ -51,9 +51,46 @@ pub trait CommandQueueInterface:
         region_size: &TileRegionSize,
     );
 
+    /// Submits an iterator of command lists for execution.
+    ///
+    /// # Arguments
+    /// * `command_lists` - The iterator of [`CommandListInterface`] command lists to be executed.
+    ///
+    /// # Remarks
+    /// Calling [`execute_command_lists`]  twice in succession (from the same thread, or different threads) guarantees that
+    /// the first workload (A) finishes before the second workload (B).
+    /// Calling [`execute_command_lists`]  with two command lists allows the driver to merge the two command lists such that the second command list (D)
+    /// may begin executing work before all work from the first (C) has finished.
+    /// Specifically, your application is allowed to insert a fence signal or wait between A and B, and the driver has no visibility into this,
+    /// so the driver must ensure that everything in A is complete before the fence operation.
+    /// There is no such opportunity in a single call to the API, so the driver is able to optimize that scenario.
+    ///
+    /// The driver is free to patch the submitted command lists.
+    /// It is the calling application’s responsibility to ensure that the graphics processing unit (GPU) is not currently reading the any of the submitted command lists from a previous execution.
+    ///
+    /// Applications are encouraged to batch together command list executions to reduce fixed costs associated with submitted commands to the GPU.
+    ///
+    /// # Runtime validation
+    /// Bundles can't be submitted to a command queue directly. If a bundle is passed to this method, the runtime will drop the call.
+    /// The runtime will also drop the call if the [`GraphicsCommandListInterface::close`](crate::command_list::GraphicsCommandListInterface::close) function has not been called on one or more of the command lists.
+    ///
+    /// The runtime will detect if the command allocators associated with the command lists have been reset after Close was called.
+    /// The runtime will drop the call and remove the device in this situation.
+    ///
+    /// The runtime will drop the call and remove the device if the command queue fence indicates that a previous execution of any of the command lists has not yet completed.
+    ///
+    /// The runtime will validate the "before" and "after" states of resource transition barriers inside of [`execute_command_lists`].
+    /// If the “before” state of a transition does not match up with the “after” state of a previous transition, then the runtime will drop the call and remove the device.
+    ///
+    /// The runtime will validate the “before” and “after” states of queries used by the command lists. If an error is detected, then the runtime will drop the call and remove the device.
+    ///
+    /// # Debug Layer
+    /// The debug layer issues errors for all cases where the runtime would drop the call.
+    ///
+    /// The debug layer issues an error if it detects that any resource referenced by the command lists, including queries, has been destroyed.
     fn execute_command_lists<'cl, I, CL>(&self, command_lists: I)
     where
-        I: Iterator<Item = &'cl CL>,
+        I: IntoIterator<Item = &'cl CL>,
         CL: CommandListInterface + 'cl;
     fn signal(&self, fence: &Fence, value: u64) -> Result<(), DxError>;
 }
@@ -96,10 +133,11 @@ impl_trait! {
 
     fn execute_command_lists<'cl, I, CL>(&self, command_lists: I)
     where
-        I: Iterator<Item = &'cl CL>,
+        I: IntoIterator<Item = &'cl CL>,
         CL: CommandListInterface + 'cl,
     {
         let command_lists = command_lists
+            .into_iter()
             .map(|l| {
                 Some(
                     l.as_raw()
