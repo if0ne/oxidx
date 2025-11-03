3e040416ea199ee9a707a82324b70e498e63c384
Pavel Agafonov <pavel.agafonov.al@gmail.com>
2025-11-03 22:14:22 +0300

    fix: build script for pix
    Signed-off-by: Pavel Agafonov <pavel.agafonov.al@gmail.com>

--- oxidx/build.rs
+++ oxidx/build.rs
@@ -1,17 +1,32 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    #[cfg(feature = "pix")]
+    copy_pix_files()?;
+
+    Ok(())
+}
+
+#[cfg(feature = "pix")]
+fn copy_pix_files() -> Result<(), Box<dyn std::error::Error>> {
     use std::env;
     use std::fs::{self};
     use std::path::PathBuf;
 
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    let link_path = PathBuf::from("./external/pix/bin/win64");
+    let out_dir = PathBuf::from(env::var("OUT_DIR")?);
 
+    let target_dir = out_dir
+        .parent()
+        .and_then(|dir| dir.parent())
+        .and_then(|dir| dir.parent())
+        .expect("failed to get target dir");
+
+    let link_path = PathBuf::from("./external/pix/bin/win64");
     let dll_file = "WinPixEventRuntime.dll";
     let lib_file = "WinPixEventRuntime.lib";
-    fs::copy(link_path.join(dll_file), out_path.join(dll_file))?;
-    fs::copy(link_path.join(lib_file), out_path.join(lib_file))?;
 
-    println!("cargo:rustc-link-search={}", out_path.display());
+    fs::copy(link_path.join(dll_file), target_dir.join(dll_file))?;
+    fs::copy(link_path.join(lib_file), target_dir.join(lib_file))?;
+
+    println!("cargo:rustc-link-search={}", target_dir.display());
     println!("cargo:rustc-link-lib=dylib=WinPixEventRuntime");
 
     Ok(())
