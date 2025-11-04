093f0fed2c6f46b7707f829f480068cced9bafa2
Pavel Agafonov <pavel.agafonov.al@gmail.com>
2025-11-04 23:00:25 +0300

    add: error type for DXGI errors
    Signed-off-by: Pavel Agafonov <pavel.agafonov.al@gmail.com>

--- oxidx/src/conv.rs
+++ oxidx/src/conv.rs
@@ -9,7 +9,7 @@ use windows::Win32::{
     },
 };
 
-use crate::{error::DxError, types::*};
+use crate::{dx::DxgiError, error::DxError, types::*};
 
 impl From<windows::core::Error> for DxError {
     fn from(value: windows::core::Error) -> Self {
@@ -20,7 +20,53 @@ impl From<windows::core::Error> for DxError {
             E_INVALIDARG => DxError::InvalidArgs,
             E_OUTOFMEMORY => DxError::Oom,
             E_NOTIMPL => DxError::NotImpl,
-            _ => DxError::Dxgi(value.message()),
+            DXGI_ERROR_ACCESS_DENIED => DxError::Dxgi(DxgiError::AccessDenied, value.message()),
+            DXGI_ERROR_ACCESS_LOST => DxError::Dxgi(DxgiError::AccessLost, value.message()),
+            DXGI_ERROR_ALREADY_EXISTS => DxError::Dxgi(DxgiError::AlreadyExists, value.message()),
+            DXGI_ERROR_CANNOT_PROTECT_CONTENT => {
+                DxError::Dxgi(DxgiError::CannotProtectContent, value.message())
+            }
+            DXGI_ERROR_DEVICE_HUNG => DxError::Dxgi(DxgiError::DeviceHung, value.message()),
+            DXGI_ERROR_DEVICE_REMOVED => DxError::Dxgi(DxgiError::DeviceRemoved, value.message()),
+            DXGI_ERROR_DEVICE_RESET => DxError::Dxgi(DxgiError::DeviceReset, value.message()),
+            DXGI_ERROR_DRIVER_INTERNAL_ERROR => {
+                DxError::Dxgi(DxgiError::DriverInternalError, value.message())
+            }
+            DXGI_ERROR_FRAME_STATISTICS_DISJOINT => {
+                DxError::Dxgi(DxgiError::FrameStatisticsDisjoint, value.message())
+            }
+            DXGI_ERROR_GRAPHICS_VIDPN_SOURCE_IN_USE => {
+                DxError::Dxgi(DxgiError::GraphicsVidpnSourceInUse, value.message())
+            }
+            DXGI_ERROR_INVALID_CALL => DxError::Dxgi(DxgiError::InvalidCall, value.message()),
+            DXGI_ERROR_MORE_DATA => DxError::Dxgi(DxgiError::MoreData, value.message()),
+            DXGI_ERROR_NAME_ALREADY_EXISTS => {
+                DxError::Dxgi(DxgiError::NameAlreadyExists, value.message())
+            }
+            DXGI_ERROR_NONEXCLUSIVE => DxError::Dxgi(DxgiError::NonExclusive, value.message()),
+            DXGI_ERROR_NOT_CURRENTLY_AVAILABLE => {
+                DxError::Dxgi(DxgiError::NotCurrentlyAvailable, value.message())
+            }
+            DXGI_ERROR_NOT_FOUND => DxError::Dxgi(DxgiError::NotFound, value.message()),
+            DXGI_ERROR_REMOTE_CLIENT_DISCONNECTED => {
+                DxError::Dxgi(DxgiError::RemoteClientDisconnected, value.message())
+            }
+            DXGI_ERROR_REMOTE_OUTOFMEMORY => DxError::Dxgi(DxgiError::RemoteOom, value.message()),
+            DXGI_ERROR_RESTRICT_TO_OUTPUT_STALE => {
+                DxError::Dxgi(DxgiError::RestrictToOutputStale, value.message())
+            }
+            DXGI_ERROR_SDK_COMPONENT_MISSING => {
+                DxError::Dxgi(DxgiError::SdkComponentMissing, value.message())
+            }
+            DXGI_ERROR_SESSION_DISCONNECTED => {
+                DxError::Dxgi(DxgiError::SessionDisconnected, value.message())
+            }
+            DXGI_ERROR_UNSUPPORTED => DxError::Dxgi(DxgiError::Unsupported, value.message()),
+            DXGI_ERROR_WAIT_TIMEOUT => DxError::Dxgi(DxgiError::WaitTimeout, value.message()),
+            DXGI_ERROR_WAS_STILL_DRAWING => {
+                DxError::Dxgi(DxgiError::WasStillDrawing, value.message())
+            }
+            _ => DxError::Other(value.message()),
         }
     }
 }
--- oxidx/src/error.rs
+++ oxidx/src/error.rs
@@ -44,10 +44,83 @@ pub enum DxError {
 
     // DXGI
     /// Generic DXGI error
-    #[error("{0}")]
-    Dxgi(String),
+    #[error("{0} {1}")]
+    Dxgi(DxgiError, String),
 
     /// Shader compilation error
     #[error("{0}")]
     ShaderCompilationError(String),
+
+    /// Unknown type of error
+    #[error("{0}")]
+    Other(String),
+}
+
+/// DXGI Errors
+///
+/// For more information: [DXGI Error](https://learn.microsoft.com/en-us/windows/win32/direct3ddxgi/dxgi-error)
+#[derive(Debug, Clone, thiserror::Error)]
+pub enum DxgiError {
+    #[error(
+        "You tried to use a resource to which you did not have the required access privileges."
+    )]
+    AccessDenied,
+    #[error("The desktop duplication interface is invalid.")]
+    AccessLost,
+    #[error("The desired element already exists.")]
+    AlreadyExists,
+    #[error("DXGI can't provide content protection on the swap chain.")]
+    CannotProtectContent,
+    #[error(
+        "The application's device failed due to badly formed commands sent by the application."
+    )]
+    DeviceHung,
+    #[error(
+        "The video card has been physically removed from the system, or a driver upgrade for the video card has occurred."
+    )]
+    DeviceRemoved,
+    #[error("The device failed due to a badly formed command.")]
+    DeviceReset,
+    #[error("The driver encountered a problem and was put into the device removed state.")]
+    DriverInternalError,
+    #[error("An event (for example, a power cycle) interrupted the gathering of presentation statistics.")]
+    FrameStatisticsDisjoint,
+    #[error("The application attempted to acquire exclusive ownership of an output, but failed because some other application (or device within the application) already acquired ownership.")]
+    GraphicsVidpnSourceInUse,
+    #[error("The application provided invalid parameter data; this must be debugged and fixed before the application is released.")]
+    InvalidCall,
+    #[error(
+        "The buffer supplied by the application is not big enough to hold the requested data."
+    )]
+    MoreData,
+    #[error(
+        "The supplied name of a resource in a call to create_shared_handle is already associated with some other resource."
+    )]
+    NameAlreadyExists,
+    #[error(
+        "A global counter resource is in use, and the Direct3D device can't currently use the counter resource."
+    )]
+    NonExclusive,
+    #[error(
+        "The resource or request is not currently available, but it might become available later."
+    )]
+    NotCurrentlyAvailable,
+    #[error("Requested object not found")]
+    NotFound,
+    #[error("TBD")]
+    RemoteClientDisconnected,
+    #[error("TBD")]
+    RemoteOom,
+    #[error("The DXGI output (monitor) to which the swap chain content was restricted is now disconnected or changed.")]
+    RestrictToOutputStale,
+    #[error("The operation depends on an SDK component that is missing or mismatched.")]
+    SdkComponentMissing,
+    #[error("The Remote Desktop Services session is currently disconnected.")]
+    SessionDisconnected,
+    #[error("The requested functionality is not supported by the device or the driver.")]
+    Unsupported,
+    #[error("The time-out interval elapsed before the next desktop frame was available.")]
+    WaitTimeout,
+    #[error("The GPU was busy at the moment when a call was made to perform an operation, and did not execute or schedule the operation.")]
+    WasStillDrawing,
 }
